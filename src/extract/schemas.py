0bed3b0c4b37b246cbf6b247ad1916141b0020e1
Ricson Ramos <98726377+RicsonRamos@users.noreply.github.com>
2026-02-21 14:25:42 -0300

    Atualizar o schemas.py

--- src/extract/schemas.py
+++ src/extract/schemas.py
@@ -1,72 +1,24 @@
 from pydantic import BaseModel, ConfigDict
 from typing import Optional, List
 
-
 class BaseAPISchema(BaseModel):
-
-    model_config = ConfigDict(
-        from_attributes=True,
-        extra="forbid"   # explode se mudar
-    )
-
-
-# -------------------------
-# RAW API MODELS
-# -------------------------
+    model_config = ConfigDict(from_attributes=True, extra="ignore")
 
 class RocketAPI(BaseAPISchema):
-
     id: str
     name: str
-    type: str
     active: bool
-
-    stages: Optional[int] = None
     cost_per_launch: Optional[int] = None
-    success_rate_pct: Optional[int] = None
-
-
-class LaunchpadAPI(BaseAPISchema):
-
-    id: str
-    full_name: str
-    status: str
-    locality: str
-    region: str
-
-    rockets: List[str] = []
-
+    success_rate_pct: Optional[float] = None # Corrigido: API envia floats
 
 class LaunchAPI(BaseAPISchema):
-
     id: str
     name: str
     date_utc: str
-
     success: Optional[bool] = None
-    flight_number: int
-
     rocket: str
-    launchpad: str
-
-
-class CrewAPI(BaseAPISchema):
-
-    id: str
-    name: str
-    agency: str
-    status: str
-
-    image: Optional[str] = None
-
-
-# -------------------------
-# REGISTRY
-# -------------------------
 
 API_SCHEMAS = {
     "rockets": RocketAPI,
-    "launches": LaunchAPI,
-    "launchpads": LaunchpadAPI,
-    "crew": CrewAPI,
+    "launches": LaunchAPI
 }
