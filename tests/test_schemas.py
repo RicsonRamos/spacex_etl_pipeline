63d0296bfc530354e6ba8142d9838f0826f3f54d
Ricson Ramos <98726377+RicsonRamos@users.noreply.github.com>
2026-02-23 20:42:58 -0300

    Update test_schemas.py

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -1,6 +1,6 @@
 import pytest
 from pydantic import ValidationError
-from src.extract.schemas import LaunchSchema # Assumindo que você usa este schema
+from src.extract.schemas import LaunchSchema
 
 def test_launch_schema_validation():
     # Dados válidos
