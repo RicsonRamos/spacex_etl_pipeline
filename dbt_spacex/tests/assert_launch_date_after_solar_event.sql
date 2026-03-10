c007e5cced19c0de1a0994e8bf36512c7624aa92
Ricson Ramos <ricsonramos@outlook.com>
2026-03-10 15:16:59 -0300

    fix: alinhamento de infra docker e correção de lógica no teste assert_launch_date_after_solar_event

--- dbt_spacex/tests/assert_launch_date_after_solar_event.sql
+++ dbt_spacex/tests/assert_launch_date_after_solar_event.sql
@@ -1,5 +1,10 @@
--- Um lançamento só pode ter um match se a data for coerente
--- Este teste retorna linhas que FALHAM na lógica
-SELECT *
-FROM {{ ref('fct_launches_performance') }}
-WHERE launch_date_utc < event_at_utc
+-- Teste Singular: Garante que não existam lançamentos registrados 
+-- ANTES de eventos solares relacionados no mesmo período.
+SELECT
+    l.launch_id,
+    l.launch_at_utc,
+    n.event_at_utc
+FROM {{ ref('fct_launches_performance') }} l
+JOIN {{ ref('stg_nasa__solar_events') }} n 
+    ON l.launch_at_utc::date = n.event_at_utc::date
+WHERE l.launch_at_utc < n.event_at_utc
